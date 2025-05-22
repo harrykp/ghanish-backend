4f09d950dba310b0f1a082033ae0a33389405185
harrykp <harrykp@users.noreply.github.com>
2025-05-22 20:36:55 +0000

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const adminOnly = require('../middleware/adminOnly');
-const Product = require('../models/Product');
 
 // Get all products
 router.get('/', async (req, res) => {
